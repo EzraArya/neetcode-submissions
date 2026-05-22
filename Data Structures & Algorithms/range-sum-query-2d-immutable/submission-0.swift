69324e599a634537a48dbeed177b533b3f7926c0
Ezra Wijaya <111065403+EzraArya@users.noreply.github.com>
2026-05-22 18:23:24 +0700

    Add: range-sum-query-2d-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-2d-immutable/submission-0.swift
@@ -0,0 +1,25 @@
+class NumMatrix {
+
+    private var matrix: [[Int]]
+
+    init(_ matrix: [[Int]]) {
+        self.matrix = matrix
+    }
+
+    func sumRegion(_ row1: Int, _ col1: Int, _ row2: Int, _ col2: Int) -> Int {
+        var res = 0
+        for r in row1...row2{
+            for c in col1...col2 {
+                res += matrix[r][c]
+            }
+        }
+
+        return res
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * let obj = NumMatrix(matrix)
+ * let ret_1: Int = obj.sumRegion(row1, col1, row2, col2)
+ */
