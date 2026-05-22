822e5f4af35c17e19ee36a504c50aa03dc8486aa
Ezra Wijaya <111065403+EzraArya@users.noreply.github.com>
2026-05-22 07:40:47 +0700

    Add: max-water-container - submission-7

---
+++ Data Structures & Algorithms/max-water-container/submission-7.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func maxArea(_ heights: [Int]) -> Int {
+        var res = 0
+        var l = 0
+        var r = heights.count - 1
+
+        while l < r {
+            let area = min(heights[l], heights[r]) * (r - l)
+            res = max(res, area)
+            if heights[l] <= heights[r] {
+                l += 1
+            } else {
+                r -= 1
+            }
+        }
+
+        return res
+    }
+}
