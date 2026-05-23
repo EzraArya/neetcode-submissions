d52e0faed0c7225b886bb457edf50600c9fb38ef
Ezra Wijaya <111065403+EzraArya@users.noreply.github.com>
2026-05-23 21:59:23 +0700

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func longestConsecutive(_ nums: [Int]) -> Int {
+        var res = 0
+        var store = Set(nums)
+
+        for num in nums {
+            var streak = 0
+            var curr = num
+
+            while store.contains(curr) {
+                streak += 1
+                curr += 1
+            }
+
+            res = max(res, streak)
+        }
+
+        return res
+    }
+}
